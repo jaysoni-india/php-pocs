c9b85e5b4e3f2b4c303b0d28e1a70885eb06ed7e
Jay Soni <me@jaysoni.dev>
2022-03-20 14:33:09 +0530

    php retriving user data from session

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,35 @@
 <?php
 
+session_start();
+
+if(count($_SESSION) > 0){
+    $name = $_SESSION['fn'];
+    $email = $_SESSION['email'];
+//    print_r($_SESSION);
+?>
+<html>
+<body>
+
+<nav>
+    <h1>
+        PHP POC
+    </h1>
+    <br>
+    <a href="./logout.php">Logout</a> (<?php echo $name; ?>, <?php echo $email; ?> )
+</nav>
+<br>
+<br>
+<div id="content">
+
+</div>
+</body>
+</html>
+
+<?php
+}else{
+    header('Location: ./register.php');
+}
+
 ?>
 
 
--- public/register_handle.php
+++ public/register_handle.php
@@ -2,14 +2,14 @@
 session_start();
 
 if (isset($_POST['submit_btn'])){
-//        $SESSION = $_POST;
+//        $_SESSION = $_POST;
         $full_name = $_POST['user_full_name'];
         $email = $_POST['user_email'];
         $password = $_POST['user_password'];
 
-        $SESSION['fn'] = $full_name;
-        $SESSION['email'] = $email;
-        $SESSION['password'] = $password;
+        $_SESSION['fn'] = $full_name;
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
         header('Location: ./dashboard.php');
 //    print_r($_POST);
 }
